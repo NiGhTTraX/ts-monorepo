f3a837d6c8b75a91a85615aea9650ef29bdbfcb3
Andrei Picus <NiGhTTraX@users.noreply.github.com>
2020-07-14 15:56:39 +0200

    feat: Add webpack example

---
+++ packages/webpack/webpack.config.js
@@ -0,0 +1,18 @@
+const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
+
+module.exports = {
+  entry: "./src/index.ts",
+  mode: 'development',
+  output: {
+    filename: "bundle.js"
+  },
+  resolve: {
+    extensions: [".ts", ".tsx", ".js"],
+    plugins: [new TsconfigPathsPlugin()]
+  },
+  module: {
+    rules: [
+      { test: /\.tsx?$/, loader: "ts-loader" }
+    ]
+  }
+};
