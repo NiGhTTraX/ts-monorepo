454122a5c8fb586ee8b7dc0797bcb94ba7cfbba4
Andrei Picus <NiGhTTraX@users.noreply.github.com>
2020-07-14 15:48:21 +0200

    chore: Use scoped package names everywhere

--- packages/nextjs/pages/index.tsx
+++ packages/nextjs/pages/index.tsx
@@ -1,6 +1,6 @@
 import React from "react";
-import {meaningOfLife} from "foo";
-import {Button} from "components";
+import {meaningOfLife} from "@nighttrax/foo";
+import {Button} from "@nighttrax/components";
 
 export default () => <div>
   <p>Meaning of life is {meaningOfLife}</p>
