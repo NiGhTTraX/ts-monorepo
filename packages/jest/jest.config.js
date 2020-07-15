4e715186480336b7356f0a584780acd7787a3d32
Andrei Picus <NiGhTTraX@users.noreply.github.com>
2020-07-15 21:28:38 +0200

    docs: Document jest.config.js

--- packages/jest/jest.config.js
+++ packages/jest/jest.config.js
@@ -1,10 +1,12 @@
 const { pathsToModuleNameMapper } = require('ts-jest/utils');
+// Load the config which holds the path aliases.
 const { compilerOptions } = require('../../tsconfig.json');
 
 module.exports = {
   preset: 'ts-jest',
 
   moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths , {
+    // This has to match the baseUrl defined in tsconfig.json.
     prefix: '<rootDir>/../../'
   })
 };
