c7796e653a83fd308d050fd4244c05921bc19e5c
Andrei Picus <NiGhTTraX@users.noreply.github.com>
2020-07-20 20:42:43 +0200

    build: Add build script for @nighttrax/components

--- examples/nextjs/pages/index.tsx
+++ examples/nextjs/pages/index.tsx
@@ -1,10 +1,4 @@
 import React from "react";
-import { meaningOfLife } from "@nighttrax/foo";
 import { Button } from "@nighttrax/components";
 
-export default () => (
-  <div>
-    <p>Meaning of life is {meaningOfLife}</p>
-    <Button />
-  </div>
-);
+export default () => <Button />;
