1eb9b1428859387fcd4c2c0ec90ff8bca1fc8a61
Andrei Picus <NiGhTTraX@users.noreply.github.com>
2021-04-19 21:02:12 +0200

    docs(next): Mention alternative for Next >= 10.1.0
    Closes #57.

--- examples/nextjs/next.config.js
+++ examples/nextjs/next.config.js
@@ -1,4 +1,12 @@
 module.exports = {
+  /**
+   * You can use the following experimental flag if you're on Next >= 10.1.0.
+   * Note that this can change/break without warning.
+   * @see https://github.com/vercel/next.js/pull/22867
+   */
+  // experimental: {
+  //   externalDir: true,
+  // },
   webpack: (config) => {
     // Let Babel compile outside of src/.
     const tsRule = config.module.rules.find(
