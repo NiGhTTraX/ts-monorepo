204b5a3fb1e8d8fca277c56a167a66e79d3865b3
Andrei Picus <NiGhTTraX@users.noreply.github.com>
2021-07-07 15:11:32 +0200

    refactor: Remove index file from components package
    Highlight how path imports can work with aliases.

--- examples/cra/src/index.tsx
+++ examples/cra/src/index.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { meaningOfLife } from "@nighttrax/foo";
-import { Button } from "@nighttrax/components";
+import { Button } from "@nighttrax/components/button";
 
 ReactDOM.render(
   <React.StrictMode>
--- examples/nextjs/pages/index.tsx
+++ examples/nextjs/pages/index.tsx
@@ -1,4 +1,4 @@
 import React from "react";
-import { Button } from "@nighttrax/components";
+import { Button } from "@nighttrax/components/button";
 
 export default () => <Button />;
