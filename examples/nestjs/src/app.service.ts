c18bec7ad597cf05fa48ca3659e7bbb37326a359
Andrei Picus <NiGhTTraX@users.noreply.github.com>
2021-10-15 16:37:41 +0200

    chore(nestjs): Silence ESLint rule

--- examples/nestjs/src/app.service.ts
+++ examples/nestjs/src/app.service.ts
@@ -3,5 +3,6 @@ import { meaningOfLife } from "@nighttrax/foo";
 
 @Injectable()
 export class AppService {
+  // eslint-disable-next-line class-methods-use-this
   getHello = (): string => `The meaning of life is: ${meaningOfLife}`;
 }
