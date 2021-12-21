78fb8727f96f8bbe899c56c1beb958de143dff7b
akutruff <874049+akutruff@users.noreply.github.com>
2021-12-21 21:29:07 +0000

    CRA tests almost working except for button import

---
+++ examples/cra/src/components/app.test.tsx
@@ -0,0 +1,9 @@
+import { App } from "./app";
+import { render, screen } from "@testing-library/react";
+import React from "react";
+
+test("renders learn react link", () => {
+  render(<App />);
+  const linkElement = screen.getByText(/learn react/i);
+  expect(linkElement).toBeInTheDocument();
+});
--- examples/cra/src/components/app.tsx
+++ examples/cra/src/components/app.tsx
@@ -10,6 +10,14 @@ export const App = () => {
     <div>
       {meaningOfLife}
       <Button />
+      <a
+        className="App-link"
+        href="https://reactjs.org"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Learn React
+      </a>
     </div>
   );
 };
